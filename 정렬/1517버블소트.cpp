e27c4cabe69deb1ba431f5228b52bd305e1ab55d
ChoMk <39166150+ChoMk@users.noreply.github.com>
2019-04-19 23:56:47 +0900

    Create 1517버블소트.cpp
    머지소트로 풀자 이때 머지 과정 중에 이동한 (스왑한) 수를 카운트하자

---
+++ 정렬/1517버블소트.cpp
@@ -0,0 +1,40 @@
+#include<cstdio>
+#include <vector>
+using namespace std;
+int N;
+int A[500003];
+long long cnt = 0;
+
+void Merge(int st, int mid, int en) {
+	vector<int> V;
+	int i = 0, j = 0, len1 = mid - st, len2 = en - mid;
+	while (i < len1 && j < len2) {
+		if (A[st + i] > A[mid + j]) {
+			cnt += len1 - i;
+			V.push_back(A[mid + j++]);
+		}
+		else
+			V.push_back(A[st + i++]);
+	}
+	while (i < len1)
+		V.push_back(A[st + i++]);
+	while (j < len2)
+		V.push_back(A[mid + j++]);
+	for (int i = st; i < en; i++)
+		A[i] = V[i - st];
+}
+void mergesort(int st, int en) {
+	if (en == st + 1) return;
+	
+	int mid = (st + en) / 2;
+	mergesort(st, mid);
+	mergesort(mid, en);
+	Merge(st, mid, en);
+}
+int main(void) {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+	mergesort(0, N);
+	printf("%lld", cnt);
+}
